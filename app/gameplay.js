2c2cd6791115b1e04cd682956ab3120c42957a5c
Max Henrique <maxhenriquealves.0@gmail.com>
2026-07-14 17:35:22 -0400

    AJUSTADO MELHOR A ROTA PARA ENTENDIMENTO E EVITAR BUGS

--- app/gameplay.js
+++ app/gameplay.js
@@ -5,9 +5,26 @@ app.use(express.json());
 
 const jogos = [];
 
+// CONTROLE DAS SOMAS DOS IDs
 let proximoId = 1;
 
-// CONTROLE DAS SOMAS DOS IDs
+// FINDINDEX -> RETORNA O INDICE (POSIÇÃO DO OBJETO DO ARRAY)
+// UTILIZADO PARA ALTERAR/REMOVER ITEM DO ARRAY
+
+// EX: [
+//   { id: 1 FIN -> UTILIZA O ID}, // índice 0 -> FINDINDEX UTILIZA O INDICE
+//   { id: 2 }, // índice 1
+//   { id: 3 }  // índice 2
+// ]
+
+// FIND -> UTILIZADO PARA VER O OBJETO INTEIRO (O ID) EX:
+// para cada jogo dentro de jogos:
+//     se jogo.id === 2:
+//         retorna esse jogo
+// É A MESMA COISA QUE FAZER -> jogos.find(function (jogo) {
+//   return jogo.id == id;
+// });
+
 const localizarId = (id) => {
   return jogos.find((jogo) => jogo.id === id);
 };
@@ -18,6 +35,8 @@ const localizarIndice = (id) => {
 
 const filtrarJogos = (genero) => {
   return jogos.filter(
+    // .toLowerCase() -> TRANSFORMA AS LETAS EM MINUSCULAS
+    // EVITA ERROS NA HORA DE REALIZAR OS FILTROS
     (filtrar) => filtrar.genero.toLowerCase() === genero.toLowerCase(),
   );
 };
@@ -26,28 +45,34 @@ app.get("/jogos", (req, res) => {
   res.status(200).send(jogos);
 });
 
-app.get("/jogos/:genero", (req, res) => {
-  const { genero } = req.params;
-  const resultadoFiltro = filtrarJogos(genero);
-  res.status(200).send(resultadoFiltro);
-});
-
-app.get("/jogos/:id", (req, res) => {
-  // NUMBER, POIS, NA HORA DE PESQUISAR VEM COMO STRING E DA NULL
+app.get("/jogos/id/:id", (req, res) => {
+  // NUMBER PQ NA HORA DE PESQUISAR VEM COMO STRING E DA NULL
   const idGame = Number(req.params.id);
   const verificarId = localizarId(idGame);
-
+  // A FORMA DE VALIDAR FIND É ASSIM
+  // CASO NAO ENCONTRE UM OBJETO RETORNA UNDEFINED
   if (verificarId === undefined) {
     return res.status(404).send("O id escolhido não existe");
   }
+  res.status(200).send(verificarId);
+});
 
-  return res.status(200).send(verificarId);
+app.get("/jogos/genero/:genero", (req, res) => {
+  const { genero } = req.params;
+  const resultadoFiltro = filtrarJogos(genero);
+  if (resultadoFiltro.length === 0) {
+    return res.send("Não há nenhum jogo com o genero escolhido");
+  }
+  res.status(200).send(resultadoFiltro);
 });
 
 app.post("/jogos", (req, res) => {
+  // EVITA FAZER UM IF DESSA FORMA ->
+  // if ((!req.body.id, !req.body.nome, !req.body.genero, !req.body.status))
   const { nome, genero, status } = req.body;
-
   if (!nome || !genero || !status) {
+    // RETURN FINALIZA A FUNCAO
+    // NECESSÁRIO PARA EVITAR BUGS
     return res.status(400).send("Está faltando informações!");
   } else {
     const criarJogo = {
@@ -56,17 +81,16 @@ app.post("/jogos", (req, res) => {
       genero: genero,
       status: status,
     };
-
     jogos.push(criarJogo);
+    // SOMA DO ID
     proximoId++;
     return res.status(201).send("Jogo adicionado");
   }
 });
 
-app.put("/jogos/:id", (req, res) => {
+app.put("/jogos/id/:id", (req, res) => {
   const indiceGame = localizarIndice(Number(req.params.id));
   const { nome, genero, status } = req.body;
-
   if (!nome || !genero || !status) {
     return res.status(400).send("Está faltando informações");
   } else {
@@ -74,23 +98,24 @@ app.put("/jogos/:id", (req, res) => {
     jogos[indiceGame].genero = genero;
     jogos[indiceGame].status = status;
   }
-
   res.status(200).send("Alteração realizada");
 });
 
-app.delete("/jogos/:id", (req, res) => {
+app.delete("/jogos/id/:id", (req, res) => {
   const indiceGame = localizarIndice(Number(req.params.id));
-
+  // A FORMA PRA VALIDAR FINDINDEX É ASSIM
+  // PRECISA UTILIZAR O -1 (É COMO SE FOSSE O UNDEFINED)
   if (indiceGame === -1) {
     return res
       .status(404)
       .send("ID não encontrado, não foi possivel deletar o jogo");
   }
-
   jogos.splice(indiceGame, 1);
   res.status(200).send("Jogo deletado!");
 });
 
 app.listen(3031, () => {
   console.log("Servidor on");
+  {
+  }
 });
